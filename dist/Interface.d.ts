ef2a0b7500b3eec2a6dc0735007b39a8d611bfca
jpbberry <jb@jt3ch.net>
2021-04-01 01:24:42 -0400

    Change to building on install

--- dist/Interface.d.ts
+++
@@ -1,6 +0,0 @@
-import { Api } from './Api';
-import { Database } from './Database';
-export declare class Interface {
-    api: Api;
-    createDb(username: string, password: string, host?: string): Database;
-}
