c0688e907f4a3296925c189f354815638d956289
jpbberry <jb@jt3ch.net>
2021-04-01 01:20:27 -0400

    default to 127.0.0.1

--- src/Interface.ts
+++ src/Interface.ts
@@ -4,7 +4,7 @@ import { Database } from './Database'
 export class Interface {
   api = new Api()
 
-  createDb (username: string, password: string, host: string) {
+  createDb (username: string, password: string, host: string = '127.0.0.1') {
     return new Database(host, username, password)
   }
 }
