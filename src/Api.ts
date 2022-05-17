1cc34bce9b2c625060c34aa52b544abf384e7cbd
Julian Berryessa <jb@bit.kiwi>
2022-05-16 21:15:56 -0400

    feat: fix @types/node

--- src/Api.ts
+++ src/Api.ts
@@ -1,11 +1,11 @@
 import fetch from 'node-fetch'
 
-import { Snowflake } from 'discord-api-types'
+import { Snowflake } from 'discord-api-types/v9'
 
 export class Api {
   url = 'https://jpbbots.org/api'
 
-  public _request (method: 'GET' | 'POST', url: string, headers?: any, body?: any) {
+  public _request(method: 'GET' | 'POST', url: string, headers?: any, body?: any) {
     return fetch(`${this.url}${url}`, {
       method,
       headers: {
@@ -16,13 +16,13 @@ export class Api {
     }).then(x => x.text())
   }
 
-  public async isAdmin (id: Snowflake) {
+  public async isAdmin(id: Snowflake) {
     const req = await this._request('GET', `/admin/${id}`)
 
     return !!Number(req)
   }
 
-  public async getPremium (id: Snowflake) {
+  public async getPremium(id: Snowflake) {
     const req = await this._request('GET', `/premium/${id}`)
 
     return Number(req)
